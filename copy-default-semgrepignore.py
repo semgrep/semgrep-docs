a536622850884533df38ed85a3d30445d16442ed
Matthew McQuaid <matthew@returntocorp.com>
2022-01-13 11:37:10 -0500

    use subprocess instead of requests to avoid the dependency

--- copy-default-semgrepignore.py
+++ copy-default-semgrepignore.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-import requests
+import subprocess
 
 FILE = "docs/cli-usage.md"
-DEFAULT = requests.get("https://raw.githubusercontent.com/returntocorp/semgrep/develop/semgrep/semgrep/templates/.semgrepignore")
+DEFAULT = subprocess.run(["curl","https://raw.githubusercontent.com/returntocorp/semgrep/develop/semgrep/semgrep/templates/.semgrepignore"],capture_output=True).stdout.decode("utf-8")
 
 with open(FILE) as file:
     filedata = file.read()
     
-filedata = filedata.replace("DEFAULT_SEMGREPIGNORE_TEXT",DEFAULT.text)
+filedata = filedata.replace("DEFAULT_SEMGREPIGNORE_TEXT",DEFAULT)
 
 with open(FILE,"w") as file:
     file.write(filedata)
