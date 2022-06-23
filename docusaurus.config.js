933053a41aed8b4c871d0339b3c8eca27d9456aa
Pablo Estrada <pablo@r2c.dev>
2022-06-23 06:31:32 -0700

    Remove office hours announcement banner (#586)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,14 +130,6 @@ module.exports = {
       //... other Algolia params
     },
     image: 'https://semgrep.dev/thumbnail.png',
-    announcementBar: {
-      id: 'office-hours',
-      content:
-        'Semgrep Weekly Wednesday Office Hours! 🕙 Every Wednesday at 10am PT. On June 22 we will dive into Autofix—<a target="_blank" rel="noopener noreferrer" href="https://get.semgrep.dev/officehours">Save your seat!</a>',
-      backgroundColor: '#5F36D9',
-      textColor: '#ffffff',
-      isCloseable: false,
-    },
   },
   scripts: [
     {
