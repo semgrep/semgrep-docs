06f47ce2cb5e3ca7ae06eafa38b3c3de6ef7f0cf
Iago Abal <mail@iagoabal.eu>
2022-09-13 15:11:36 +0200

    experiments: Add taint labels (#698)
    * experiments: Add taint labels
    
    * Apply suggestions from code review
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -74,6 +74,7 @@ module.exports = {
             'experiments/project-depends-on',
             'experiments/symbolic-propagation',
             'experiments/taint-propagators',
+            'experiments/taint-labels',
             'experiments/metavariable-analysis'
           ]
         },
