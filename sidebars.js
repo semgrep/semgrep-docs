36fdebf3a1d6239b42bde4907305a9aac9a2f26a
s-santillan <sarasantillan@gmail.com>
2022-11-05 03:26:42 +0800

    remove ssc supported languages

--- sidebars.js
+++ sidebars.js
@@ -136,7 +136,6 @@ module.exports = {
             'semgrep-sc/sc-getting-started',
             'semgrep-sc/sc-triage-remediation',
             'semgrep-sc/sc-ignoring-deps',
-            'semgrep-sc/sc-supported-langs',
             'semgrep-sc/sc-glossary'
         ]
     },
