7293fb6e4a8cb356eb192d63999496a6a7648809
s-santillan <sarasantillan@gmail.com>
2022-11-05 03:11:37 +0800

    removed ssc and created redirects

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -214,7 +214,8 @@ module.exports = {
           { from: "/experiments/metavariable-analysis/", to: "/writing-rules/experiments/metavariable-analysis/" },
           { from: "/experiments/multiple-focus-metavariables/", to: "/writing-rules/experiments/multiple-focus-metavariables/" },
           { from: "/experiments/display-propagated-metavariable/", to: "/writing-rules/experiments/display-propagated-metavariable/" },
-          { from: "/experiments/deprecated-experiments/", to: "/writing-rules/experiments/deprecated-experiments/" }
+          { from: "/experiments/deprecated-experiments/", to: "/writing-rules/experiments/deprecated-experiments/" },
+          { from: "/semgrep-sc/sc-supported-langs/", to: "/supported-languages/" }
         ]
       }
     ],
