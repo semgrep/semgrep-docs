525791e651885f014f509cc67f3155c11b84861b
adamkvitek <adamkvitek@gmail.com>
2022-12-16 15:42:58 +0100

    Add ruby code highlighting, minor edit in playground docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ module.exports = {
     prism: {
       theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
-      additionalLanguages: ['java'],
+      additionalLanguages: ['java', 'ruby'],
     },
     algolia: {
       apiKey: 'f53612c29d04a2ff71dce6e3b2f76752',
