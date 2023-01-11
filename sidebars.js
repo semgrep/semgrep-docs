9b538f958306e3c6da7c8ce529159215f14b264d
Pieter De Cremer (r2c) <pieter@r2c.dev>
2023-01-11 20:12:01 +0100

    Adding Java XXE Cheatsheet (#804)
    * Adding Java XXE Cheatsheet
    
    * add all other tables
    
    * brief descriptions of different security measures
    
    * Include XXE in the sidebar
    
    * specify for which Java versions we tested this
    
    * Restructure the headers, write an intro for table super section
    
    * Add code tags
    
    * testing some layouts
    
    * making tables
    
    * updates to tables
    
    * finished all tables:
    
    * fixed typo
    
    * Fix sidebar
    
    * updated attack payloads so that no copied content is present
    
    * added link to rules
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    * n/a -> N/A, Team tier -> Teams tier
    
    * more updates to the java XXE cheatsheet
    
    * Teams tier -> Team tier
    
    * Add infographics
    
    * Minor addition
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    
    * Elevate Overview of the effect of security measures section to higher header as it includes XML bomb also, minor edits
    
    * Minor edits
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    * Update java-xxe.mdx
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    * Trigger Build
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    
    * small updates based on comments by tpetr
    
    * Trigger Build
    
    * Update infographics
    
    * add introduction
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    
    * Update docs/cheat-sheets/java-xxe.mdx
    
    * update infographic
    
    * Edit introduction, include higher resolution infographics, add example of handling Exponential entity expansion
    
    * Fix typos
    
    * Apply suggestions from code review
    
    Co-authored-by: s-santillan <sarasantillan@gmail.com>
    Co-authored-by: adamkvitek <adamkvitek@gmail.com>
    Co-authored-by: s-santillan <sarasantillan@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -135,6 +135,13 @@ module.exports = {
                             'cheat-sheets/rails-xss'
                         ]
                     },
+                    {
+                        type: 'category',
+                        label: 'XXE',
+                        items: [
+                            'cheat-sheets/java-xxe',
+                        ]
+                    },
                 ],
             },
         ]
