3971f6c39d3314d26b8299a967c89f03b562a001
adamkvitek <adamkvitek@gmail.com>
2023-05-12 16:28:50 +0200

    Use Semgrep, Inc in rule updates, and returntocorp as a proper name in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ module.exports = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'semgrep', // Usually your GitHub org/user name.
+  organizationName: 'returntocorp', // Usually your GitHub org/user name.
   projectName: 'semgrep', // Usually your repo name.
   trailingSlash: true,
   themeConfig: {
