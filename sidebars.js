f6f25419b14d7519241ac807398a5be6abb93600
adamkvitek <adamkvitek@gmail.com>
2023-06-07 12:49:12 +0200

    Order experiments alphabetically in sidebar (but keep deprecated experiments as the last item), add Aliengrep to sidebar

--- sidebars.js
+++ sidebars.js
@@ -97,6 +97,9 @@ module.exports = {
                     label: 'Experiments 🧪',
                     link: {type: 'doc', id: 'writing-rules/experiments/introduction'},
                     items: [
+                      'writing-rules/experiments/aliengrep',
+                      'writing-rules/experiments/display-propagated-metavariable',
+                      'writing-rules/experiments/extract-mode',
                       { type: 'category',
                           label: 'Join mode',
                           link: {type: 'doc', id: 'writing-rules/experiments/join-mode/overview'},
@@ -109,12 +112,10 @@ module.exports = {
                               'writing-rules/experiments/join-mode/recursive-joins'
                           ]
                       },
-                      'writing-rules/experiments/extract-mode',
+                      'writing-rules/experiments/multiple-focus-metavariables',
                       'writing-rules/experiments/project-depends-on',
                       'writing-rules/experiments/symbolic-propagation',
                       'writing-rules/experiments/taint-labels',
-                      'writing-rules/experiments/multiple-focus-metavariables',
-                      'writing-rules/experiments/display-propagated-metavariable',
                       'writing-rules/experiments/deprecated-experiments'
                     ]
                   },
