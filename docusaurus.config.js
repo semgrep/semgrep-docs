78135f02f7926253d0e6243e4eb282454f55de6d
s-santillan <sarasantillan@gmail.com>
2023-07-07 16:31:56 +0800

    add key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,9 @@ module.exports = {
         hideable: true,
       },
     },
+    markprompt: {
+      projectKey: 'jbhF5LligltdKaJucMjDcWcRodaVpzqE',
+    },
     navbar: {
       logo: {
         alt: 'Semgrep logo',
