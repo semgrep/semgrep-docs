ddb824f747d928da40dd01f1336f64073d4dfc90
s-santillan <sarasantillan@gmail.com>
2023-07-11 07:52:35 +0800

    Sara/experiment/markprompt (#1110)
    * use typescript
    
    * wrap searchbar, install markprompt
    
    * correct SearchBar wrapper element
    
    * add key
    
    * update markprompt component

---
+++ src/theme/SearchBar/index.tsx
@@ -0,0 +1,41 @@
+import type { WrapperProps } from '@docusaurus/types';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import type SearchBarType from '@theme/SearchBar';
+import SearchBar from '@theme-original/SearchBar';
+import React, { useEffect } from 'react';
+
+import '@markprompt/css';
+
+type Props = WrapperProps<typeof SearchBarType>;
+
+export default function SearchBarWrapper(props: Props): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+
+  useEffect(() => {
+    (async () => {
+      const markprompt = (await import('@markprompt/web')).markprompt;
+      const { projectKey, ...config } = siteConfig.themeConfig
+        .markprompt as any;
+      markprompt(projectKey, '#markprompt', {
+        ...config,
+        references: {
+          transformReferenceId: (referenceId) => {
+            // Sample code that transforms a reference path to a link.
+            // Remove file extension
+            const href = referenceId.replace(/\.[^.]+$/, '');
+            // Use last part of path for label
+            const text = href.split('/').slice(-1)[0];
+            return { text, href };
+          },
+        },
+      });
+    })();
+  }, [siteConfig.themeConfig.markprompt]);
+
+  return (
+    <div style={{ display: 'flex', gap: '16px', alignItems: 'center' }}>
+      <div id="markprompt" />
+      <SearchBar {...props} />
+    </div>
+  );
+}
