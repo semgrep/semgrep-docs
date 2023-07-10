51e28afb0ca41068a9879e17c6be0e40a7ab4930
s-santillan <sarasantillan@gmail.com>
2023-07-11 06:59:54 +0800

    update markprompt component

--- src/theme/SearchBar/index.tsx
+++ src/theme/SearchBar/index.tsx
@@ -1,6 +1,5 @@
 import type { WrapperProps } from '@docusaurus/types';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { markprompt } from '@markprompt/web';
 import type SearchBarType from '@theme/SearchBar';
 import SearchBar from '@theme-original/SearchBar';
 import React, { useEffect } from 'react';
@@ -13,20 +12,24 @@ export default function SearchBarWrapper(props: Props): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
 
   useEffect(() => {
-    const { projectKey, ...config } = siteConfig.themeConfig.markprompt as any;
-    markprompt(projectKey, '#markprompt', {
-      ...config,
-      references: {
-        transformReferenceId: (referenceId) => {
-          // Sample code that transforms a reference path to a link.
-          // Remove file extension
-          const href = referenceId.replace(/\.[^.]+$/, '');
-          // Use last part of path for label
-          const text = href.split('/').slice(-1)[0];
-          return { text, href };
+    (async () => {
+      const markprompt = (await import('@markprompt/web')).markprompt;
+      const { projectKey, ...config } = siteConfig.themeConfig
+        .markprompt as any;
+      markprompt(projectKey, '#markprompt', {
+        ...config,
+        references: {
+          transformReferenceId: (referenceId) => {
+            // Sample code that transforms a reference path to a link.
+            // Remove file extension
+            const href = referenceId.replace(/\.[^.]+$/, '');
+            // Use last part of path for label
+            const text = href.split('/').slice(-1)[0];
+            return { text, href };
+          },
         },
-      },
-    });
+      });
+    })();
   }, [siteConfig.themeConfig.markprompt]);
 
   return (
