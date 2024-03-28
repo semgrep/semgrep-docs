b481dbee6cc7d0aa88d84258755368a927097653
Iago Abal <mail@iagoabal.eu>
2024-03-28 19:51:08 +0100

    Update taint docs (#1446)
    * Update taint docs
    
    Closes FLOW-51
    Closes FLOW-52
    Closes FLOW-53
    Closes FLOW-54
    Closes FLOW-55
    Closes FLOW-60
    Closes FLOW-64
    
    * Apply suggestions from code review
    
    Co-authored-by: s-santillan <sarasantillan@gmail.com>
    
    * Update docs/writing-rules/data-flow/taint-mode.md
    
    ---------
    
    Co-authored-by: s-santillan <sarasantillan@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -342,7 +342,6 @@ module.exports = {
                       'writing-rules/experiments/multiple-focus-metavariables',
                       'writing-rules/experiments/project-depends-on',
                       'writing-rules/experiments/symbolic-propagation',
-                      'writing-rules/experiments/taint-labels',
                       'writing-rules/experiments/metavariable-type',
                       'writing-rules/experiments/deprecated-experiments'
                     ]
