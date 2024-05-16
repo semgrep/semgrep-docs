69b350b83f802331bd91a6ff077eace4e15ff184
s-santillan <sarasantillan@gmail.com>
2024-05-17 00:41:35 +0800

    begin creating top-level tag

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,3 +1,37 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import clsx from 'clsx';
+import Tag from '@theme/Tag';
+import {useDoc} from '@docusaurus/theme-common/internal';
+
+export default function Tags({tag}) {
+
+const {metadata} = useDoc();
+  const {editUrl, lastUpdatedAt, lastUpdatedBy, tags} = metadata;
+  return (
+    <>
+      <ul className={clsx('padding--none', 'margin-left--sm')}>
+        {tags.map(({label, permalink: tagPermalink}) => (
+          <li key={tagPermalink}>
+            <Tag label={label} permalink={tagPermalink} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+
+
+
+
+/*
 import React from 'react';
 import TagsListInline from '@theme/TagsListInline';
 import {useDoc} from '@docusaurus/theme-common/internal';
@@ -11,3 +45,4 @@ const {metadata} = useDoc();
            <TagsListInline tags={tags} />
   );
 }
+*/
