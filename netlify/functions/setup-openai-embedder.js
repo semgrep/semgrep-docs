c83029535b72bcc3edcf1871c9b88c6d28ee9660
Abhijna Parigi <Abhijna@MacBook-Pro.local>
2025-10-14 14:19:22 -0700

    search key

--- netlify/functions/setup-openai-embedder.js
+++ netlify/functions/setup-openai-embedder.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
   try {
     const client = new MeiliSearch({
       host: process.env.MEILISEARCH_HOST_URL || 'https://ms-0e8ae24505f7-30518.sfo.meilisearch.io',
-      apiKey: process.env.MEILISEARCH_API_KEY // API key must be set in environment variables
+      apiKey: process.env.MEILISEARCH_API_KEY || '3b0bb96d01687d3647a8de882c7110e8b60b1b7006719936d530ea8b6a72a2f8'
     });
 
     // Create OpenAI embedder
